7dc7a3f31355207b59b24fb769f4d3e1142c2181
jaczhi <54284925+jaczhi@users.noreply.github.com>
2024-10-20 14:31:04 -0700

    Add OIDC login

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { oidcAuthMiddleware, getAuth, revokeSession, processOAuthCallback } from '@hono/oidc-auth'
 import * as kp from './key-provider'
-import {HTTPException} from "hono/http-exception";
 
 const app = new Hono()
 
 app.get('/', async (c) => {
+  const auth = await getAuth(c);
+  if (auth)
+    return c.text(`Hello <${auth?.email}>!`);
+  else
+    return c.text("Hello World! You are not logged in.");
+})
+
+app.get('/keys', async (c) => {
+  const auth = await getAuth(c);
+  if (!auth)
+    return c.json({error: "Not authorized"}, 403);
+
+  // Dummy code to make a key and return all keys
   const newRow: kp.KeyInfo = {
     Sequence: 3,
     Expiration: Math.floor(Date.now() / 1000), // current timestamp in seconds
@@ -19,9 +33,21 @@ app.get('/', async (c) => {
   return new Promise((resolve, reject) => {
     kp.readKeys()
         .then(keys => resolve(c.json(keys)))
-        .catch(err => resolve(c.json({error: err})))
+        .catch(err => resolve(c.json({error: err}, 500)))
   });
 })
+
+app.get('/logout', async (c) => {
+  await revokeSession(c)
+  return c.text('You have been successfully logged out!')
+})
+
+app.get('/callback', async (c) => {
+  return processOAuthCallback(c)
+})
+
+app.use('*', oidcAuthMiddleware())
+
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
