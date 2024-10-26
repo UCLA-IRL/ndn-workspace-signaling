e5127e98e3d2a045a8cc2f478786919271209870
jaczhi <54284925+jaczhi@users.noreply.github.com>
2024-10-25 17:24:32 -0700

    invitation code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
+import axios from 'axios';
+import {randomBytes} from "crypto";
 import { serve } from '@hono/node-server'
+import { validator } from 'hono/validator';
 import { Hono } from 'hono'
 import { oidcAuthMiddleware, getAuth, revokeSession, processOAuthCallback } from '@hono/oidc-auth'
 import * as kp from './key-provider'
@@ -25,6 +28,91 @@ app.get('/keys', async (c) => {
         .catch(err => resolve(c.json({error: err}, 500)))
   });
 })
+
+app.post('/invite',
+    validator('json', (value, c) => {
+      const email = value['email'];
+
+      if (email == undefined || email === '') {
+        c.json({error: 'email is required'}, 400);
+      }
+
+      return { email };
+    }),
+    async (c) => {
+      const auth = await getAuth(c);
+      // @ts-ignore
+      if (!auth || !process.env.ADMIN_USERS.split(',').map(e => e.trim()).includes(auth?.email))
+        return c.json({error: "Not authorized"}, 403);
+
+      const { email } = c.req.valid('json');
+
+      const authUrl = process.env.OIDC_ISSUER + '/oauth/token';
+      const audience = process.env.OIDC_ISSUER + '/api/v2/';
+      const createUserUrl = process.env.OIDC_ISSUER + '/api/v2/users';
+      const changePasswordUrl = process.env.OIDC_ISSUER + '/dbconnections/change_password';
+
+      try {
+        // Step 1: Obtain access token
+        const tokenResponse = await axios.post(authUrl, new URLSearchParams({
+            grant_type: 'client_credentials',
+            client_id: process.env.OIDC_CLIENT_ID,
+            client_secret: process.env.OIDC_CLIENT_SECRET,
+            audience: audience,
+          } as unknown as undefined),
+            {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' }
+            }
+        );
+
+        const accessToken = tokenResponse.data.access_token;
+
+        // Step 2: Create user
+        const createUserResponse = await axios.post(
+            createUserUrl,
+            {
+              email: email,
+              email_verified: true,
+              name: email,
+              connection: process.env.AUTH0_CONNECTION_NAME,
+              password: randomBytes(32).toString('hex') + "aA1!" /* comply with any password req */,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
+              },
+            }
+        );
+
+        // Step 3: Initiate password change request, if user creation succeeded
+        if (createUserResponse.status === 201) {
+          const changePasswordResponse = await axios.post(
+              changePasswordUrl,
+              {
+                client_id: process.env.OIDC_CLIENT_ID,
+                connection: process.env.AUTH0_CONNECTION_NAME,
+                email: email,
+              },
+              {
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                  'Authorization': `Bearer ${accessToken}`,
+                },
+              }
+          );
+
+          return c.text(changePasswordResponse.data)
+        } else {
+          return c.json(createUserResponse.data, 500);
+        }
+      } catch (err) {
+        return c.json({error: err}, 500);
+      }
+  }
+)
 
 app.get('/logout', async (c) => {
   await revokeSession(c)
